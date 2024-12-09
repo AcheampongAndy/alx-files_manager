5fa2525240df42dd41487e6a8ed33cb956fd03cf
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-12-09 12:07:57 +0000

    redis clien

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,8 @@
 /** 
  * Importing Libraries 
  * */
-import { promisify } from 'utils';
-import { createClient } from 'radis';
+import { promisify } from 'util';
+import { createClient } from 'redis';
 
 /** 
  * A class that represent the client side 
@@ -23,52 +23,50 @@ class RedisClient {
             this.isClientConnected = true;
         });
     };
+
+
+    /** 
+     * a function isAlive that returns true 
+     * when the connection to Redis is a success 
+     * otherwise, false
+     */
+
+    isAlive() {
+        return this.isClientConnected;
+    }
+
+    /**
+     * an asynchronous function get that takes 
+     * a string key as argument and 
+     * returns the Redis value stored for this key
+     */
+
+    async get(key) {
+        return promisify(this.client.get).bind(this.client)(key);
+    }
+
+    /**
+     * an asynchronous function set that takes 
+     * a string key, a value and a duration in 
+     * second as arguments to store it in Redis 
+     * (with an expiration set by the duration argument)
+     */
+    async set(key, duration, value) {
+        await promisify(this.client.setex)
+        .bind(this.client)(key, duration, value);
+    }
+
+    /**
+     * an asynchronous function del 
+     * that takes a string key as argument 
+     * and remove the value in Redis for this key
+     */
+    async del(key) {
+        await promisify(this.client.del)
+        .bind(this.client)(key);
+    }
+
 }
 
-/** 
- * a function isAlive that returns true 
- * when the connection to Redis is a success 
- * otherwise, false
- */
-
-isAlive() {
-    return this.isClientConnected;
-}
-
-/**
- * an asynchronous function get that takes 
- * a string key as argument and 
- * returns the Redis value stored for this key
- */
-
-async get(key) {
-    return promisify(this.client.GET).bind(this.client)(key);
-}
-
-/**
- * an asynchronous function set that takes 
- * a string key, a value and a duration in 
- * second as arguments to store it in Redis 
- * (with an expiration set by the duration argument)
- */
-async set(key, value, duration) {
-    await promisify(this.client.SETEX)
-    .bind(this.client)(key, value, duration);
-}
-
-/**
- * an asynchronous function del 
- * that takes a string key as argument 
- * and remove the value in Redis for this key
- */
-async del(key) {
-    await promisify(this.client.DEL)
-    .bind(this.client)(key);
-}
-
-export redisClient = new RedisClient();
+const redisClient = new RedisClient();
 export default redisClient;
-
-
-
-
