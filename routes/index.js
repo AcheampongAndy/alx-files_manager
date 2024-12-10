298a32fb1cc3dd017e2c298bd1e7ab057e15e6d2
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-12-10 18:50:51 +0000

    files

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,6 @@ router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
+router.get('/files', FilesController.getIndex); 
 
 export default router;
